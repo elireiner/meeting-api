9c08aa57cb88a06812b7ccd0e939ee0699651574
Eli Reiners <goelyukim@gmail.com>
2020-09-24 20:24:34 -0400

    Add dotenv

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,11 @@
+process.env.NODE_ENV = 'test'
+process.env.TZ = 'UTC'
+
+require('dotenv').config()
+
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+  || 'postgresql://dunder_mifflin@localhost/meetings-test'
+  
 const { expect } = require('chai');
 const supertest = require('supertest');
 
