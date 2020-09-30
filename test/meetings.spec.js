f26cacf6817ee54b022e4b71763414c6819471bf
Eli Reiners <goelyukim@gmail.com>
2020-09-30 14:40:19 -0400

    Add test for post

--- test/meetings.spec.js
+++ test/meetings.spec.js
@@ -35,4 +35,23 @@ describe('/api/ meetings', () => {
             })
         })
     })
+    describe('POST /api/meetings', () => {
+        context('When the newMeeting has all fields', () => {
+            const newMeeting = {
+                "_id": "1903cce2-ff22-463b-b45d-06cb6e671880",
+                "meeting_name": "Plan",
+                "meeting_type": "Yearly",
+                "description": "Mollit ad labore commodo est sit mollit. Lorem ad sint et quis esse est magna aliqua id. Excepteur sint reprehenderit in excepteur exercitation. Elit minim consequat consequat laborum incididunt enim elit laborum consectetur reprehenderit irure amet culpa enim. Adipisicing enim ad exercitation excepteur anim labore culpa sit et consectetur occaecat dolore sunt. Adipisicing sint ut incididunt aliqua tempor aliquip.",
+                "meeting_time": "5/6/2018, 9:33:06 PM"
+            }
+
+            it('responds with 201', () => {
+                return supertest(app)
+                    .post('/api/meetings')
+                    .set({ 'Authorization': `Bearer ${process.env.API_TOKEN}` })
+                    .send(newMeeting)
+                    .expect(201)
+            })
+        })
+    })
 })
