a2d12b3a12890318d4d891a633c08889d2a44f37
Eli Reiners <goelyukim@gmail.com>
2020-10-02 15:26:13 -0400

    Change to correct table name

--- seeds/seed.user_team.sql
+++ seeds/seed.user_team.sql
@@ -1,3 +1,3 @@
-insert into users (user_id, team_id) values (1, 1);
-insert into users (user_id, team_id) values (1, 2);
-insert into users (user_id, team_id) values (1, 3);
+insert into user_team (user_id, team_id) values (1, 1);
+insert into user_team (user_id, team_id) values (1, 2);
+insert into user_team (user_id, team_id) values (1, 3);
