7a24e477acd57b09cf4f6abc9a863c8febab708b
Eli Reiners <goelyukim@gmail.com>
2020-10-02 14:59:36 -0400

    Remove unneeded space

--- test/meetings.spec.js
+++ test/meetings.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const app = require('../src/app');
 const supertest = require('supertest');
 
-describe('/api/ meetings', () => {
+describe('/api/meetings', () => {
 
     let db;
 
