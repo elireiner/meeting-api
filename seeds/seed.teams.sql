fc3c91ed9148095369107c146b24d6973426abc1
Eli Reiners <goelyukim@gmail.com>
2020-10-02 14:55:41 -0400

    Initial commit

---
+++ seeds/seed.teams.sql
@@ -0,0 +1,9 @@
+INSERT INTO teams(_id,teams_name, active) VALUES ('fa67ea53-346a-4597-bad8-a037ddd92198','Champions',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('311d0f19-f28a-49ae-bd4e-7eee17d83c5a','Heart Warmers',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('55480445-ec32-4567-a5a0-0c2c7e2eb246','Rustic Passion',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('6e7cf70e-c4ec-4434-b795-e24b69dc9a30','Prosper Gurus',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('4dbea789-1285-4717-bf09-5b64a67307fa','Innovation Geeks',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('d41bfa6d-cbdb-4fff-adbd-4c09c6868e41','Quest Pirates',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('f44637e2-14af-497e-93d3-5f97c4307d1f','Miracle Workers',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('c4f7649c-1ed1-409d-a376-dcf8536d6bab','Diva Drive',true);
+INSERT INTO teams(_id,teams_name, active) VALUES ('31a0a0b3-d8bb-473c-9c28-a683ebd9206a','Mind Benders',true);
