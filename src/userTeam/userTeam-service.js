6085c19247196613bea440f9d192989c5094afcf
Eli Reiners <goelyukim@gmail.com>
2020-10-05 06:46:32 -0400

    Add methods

--- src/userTeam/userTeam-service.js
+++ src/userTeam/userTeam-service.js
@@ -3,10 +3,31 @@ const userTeamService = {
     getAllTeams(knex) {
         return knex.select('*').from('teams')
     },
-// TODO: dynamically use the ud
+// TODO: dynamically use the id
     getById(knex, id) {
         return knex.raw(`SELECT * FROM user_team ut INNER JOIN teams t on ut.team_id = t.team_id WHERE ut.user_id = 1`)
     },
+    insertTeam(knex, newTeam) {
+        return knex
+            .insert(newTeam)
+            .into('teams')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    insertUserTeam(knex, newUserTeam) {
+        return knex
+            .insert(newUserTeam)
+            .into('user_team')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteAllTeams(knex) {
+        return knex.raw(`TRUNCATE TABLE teams RESTART IDENTITY CASCADE`)
+    }
 }
 
 module.exports = userTeamService
