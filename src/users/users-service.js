b8f9e2d4b67ba9ffd8d2c9ea8146b026e30f5239
Eli Reiners <goelyukim@gmail.com>
2020-10-05 06:46:15 -0400

    Add methods

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -4,27 +4,31 @@ const UsersService = {
         return knex.select('*').from('users')
     },
 
-    /*insertUser(knex, newUser) {
+    insertUser(knex, newUser) {
         return knex
             .insert(newUser)
-            .into('noteful_users')
+            .into('users')
             .returning('*')
             .then(rows => {
                 return rows[0]
             })
-    },*/
+    },
 
     getById(knex, id) {
         return knex.from('users').select('*').where('user_id', id).first()
     },
 
-    /*deleteUser(knex, id) {
-        return knex('noteful_users')
+    deleteUser(knex, id) {
+        return knex('users')
             .where({ id })
             .delete()
     },
 
-    updateUser(knex, id, newUserFields) {
+    deleteAllUsers(knex) {
+        return knex.raw(`TRUNCATE TABLE users RESTART IDENTITY CASCADE`)
+    }
+
+   /* updateUser(knex, id, newUserFields) {
         return knex('noteful_users')
             .where({ id })
             .update(newUserFields)
