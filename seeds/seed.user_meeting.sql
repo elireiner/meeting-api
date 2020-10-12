0c44b85fceb67ea33c1466a8438cabfff1ac0570
Eli Reiners <goelyukim@gmail.com>
2020-10-12 13:57:52 -0400

    Initial commit

---
+++ seeds/seed.user_meeting.sql
@@ -0,0 +1,3 @@
+insert into user_meeting (user_id, meeting_id) values (1, 12);
+insert into user_meeting (user_id, meeting_id) values (1, 13);
+insert into user_meeting (user_id, meeting_id) values (2, 12);
