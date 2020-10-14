55767d200f4d47723f644adb0fc36f169f8a4f55
Eli Reiners <goelyukim@gmail.com>
2020-10-14 13:34:02 -0400

    Initial commit

---
+++ seeds/seed.assesment.sql
@@ -0,0 +1,8 @@
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 1, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 2, 5, 1, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 3, 5, 1, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 4, 5, 1, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 5, 5, 1, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 2, 1);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 1, 2);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 1, 3);
