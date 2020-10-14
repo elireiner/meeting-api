77581ca3f19dd5a1785e849aed98d36fc2aeae23
Eli Reiners <goelyukim@gmail.com>
2020-10-14 13:34:20 -0400

    Fix refrences

--- seeds/seed.user_meeting.sql
+++ seeds/seed.user_meeting.sql
@@ -1,3 +1,3 @@
-insert into user_meeting (user_id, meeting_id) values (1, 12);
-insert into user_meeting (user_id, meeting_id) values (1, 13);
-insert into user_meeting (user_id, meeting_id) values (2, 12);
+insert into user_meeting (user_id, meeting_id) values (1, 1);
+insert into user_meeting (user_id, meeting_id) values (1, 2);
+insert into user_meeting (user_id, meeting_id) values (2, 1);
