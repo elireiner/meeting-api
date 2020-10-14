39239a85244bd17d4ab796cd0046d0c41b2d93c0
Eli Reiners <goelyukim@gmail.com>
2020-10-14 13:38:46 -0400

    Add another insert

--- seeds/seed.assesment.sql
+++ seeds/seed.assesment.sql
@@ -6,3 +6,4 @@ insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values
 insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 2, 1);
 insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 1, 2);
 insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 5, 1, 3);
+insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) values ('909435a1-4ab8-4245-8634-6c75010520de', 1, 2, 1, 1);
