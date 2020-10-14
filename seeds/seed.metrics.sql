a15766946070616027ed14d2435d85b42d49ba7c
Eli Reiners <goelyukim@gmail.com>
2020-10-14 13:34:42 -0400

    Improve questions

--- seeds/seed.metrics.sql
+++ seeds/seed.metrics.sql
@@ -1,5 +1,5 @@
 INSERT INTO metrics(_id,metric_name) VALUES ('81d2cf1a-84d0-4852-aba6-ab410a3f2e57','How nessecery was this meeting?');
-INSERT INTO metrics(_id,metric_name) VALUES ('b3776526-15e6-4666-bbc5-05ad9df5654a','Befoe the meeting, how clear were the desired outcomes?');
-INSERT INTO metrics(_id,metric_name) VALUES ('aca99382-1d94-48d4-af78-1de2b129f81f','How well did with stay on point?');
-INSERT INTO metrics(_id,metric_name) VALUES ('828e01df-7bc3-4649-8d0d-ab4fc15c6e65','How well did we summarize and agreed on next steps?');
-INSERT INTO metrics(_id,metric_name) VALUES ('80730081-6abd-4846-a6e9-8428f2e2bad7','How strongly was open discussion encouraged?');
+INSERT INTO metrics(_id,metric_name) VALUES ('b3776526-15e6-4666-bbc5-05ad9df5654a','Befoe the meeting, how well did we clearify the desired outcomes?');
+INSERT INTO metrics(_id,metric_name) VALUES ('aca99382-1d94-48d4-af78-1de2b129f81f','How well did we stay on point?');
+INSERT INTO metrics(_id,metric_name) VALUES ('828e01df-7bc3-4649-8d0d-ab4fc15c6e65','How well did we summarize and agree on next steps?');
+INSERT INTO metrics(_id,metric_name) VALUES ('80730081-6abd-4846-a6e9-8428f2e2bad7','How strongly did we encourage open discussion?');
