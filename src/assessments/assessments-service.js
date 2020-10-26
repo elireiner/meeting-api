b7b78abf14ef1a12da23509a1fec2993dba051a2
Eli Reiners <goelyukim@gmail.com>
2020-10-26 10:45:28 -0400

    add getUsersMeetingsAssessAvg service method

--- src/assessments/assessments-service.js
+++ src/assessments/assessments-service.js
@@ -1,5 +1,5 @@
 const AssessmentsService = {
-    
+
     getAllAssessments(knex) {
         return knex.select('*').from('assesment')
     },
@@ -7,27 +7,42 @@ const AssessmentsService = {
     insertAssessment(knex, newAssessment) {
         return knex.raw(`insert into assesment (_id, metric_id, metric_value, user_id, meeting_id) 
         values (:_id, :metric_id, :metric_value, :user_id, :meeting_id) RETURNING *;`, newAssessment)
-         
+
     },
 
     getAllAssessmentsForUser(knex, id) {
         return knex.raw(`select assesment.assesment_pkey, assesment._id, metrics.metric_id, metrics.metric_name, assesment.metric_value, assesment.meeting_id from assesment inner join metrics on assesment.metric_id=metrics.metric_id where user_id = ? order by meeting_id;`, [id])
     },
 
-    getAllAssessmentsForMeeting(knex, id) {
-        return knex.raw(`select assesment.assesment_pkey, assesment._id, metrics.metric_id, metrics.metric_name, assesment.metric_value, assesment.meeting_id from assesment inner join metrics on assesment.metric_id=metrics.metric_id where meeting_id = ?;`, [id])
+    getUserAssessmentForMeeting(knex, userId, meetingId) {
+        return knex.raw(`select * from assesment where user_id = :userId: and meeting_id = :meetingId:;`, {
+            userId, meetingId
+        })
     },
 
+    // get the average for each meeting a user was a participant
+    getUsersMeetingsAssessAvg(knex, id) {
+  
+        const subColumn = knex.raw('select distinct meeting_id from user_meeting where user_meeting.user_id = ?', [id])
+            .wrap('(', ')');
+
+        return knex('assesment')
+            .select(knex.raw(`assesment.meeting_id, assesment.metric_id, avg(metric_value)::NUMERIC(10,2) as average`))
+            .where(knex.raw(`assesment.meeting_id in :subColumn:`, {subColumn}))
+            .groupBy(knex.raw(`assesment.metric_id, assesment.meeting_id`))
+            .orderBy(knex.raw(`assesment.meeting_id, assesment.metric_id`))
+    },
+
+    // get the cumulative average of each recurring meeting a user was a participant
     getUsersRecurringMeetingsAssessAvg(knex, id) {
-        return knex.raw(`select user_meeting.user_id as current_user, assesment.meeting_id, assesment.metric_id, metrics.metric_name, avg(metric_value)::NUMERIC(10,2)
-        from metrics
-        inner join assesment 
-          on metrics.metric_id=assesment.metric_id 
-        inner join user_meeting 
-          on  assesment.meeting_id=user_meeting.meeting_id  
-        where user_meeting.user_id = ?
-        group by assesment.metric_id, assesment.meeting_id, metrics.metric_name, user_meeting.user_id, user_meeting.meeting_id
-        order by assesment.meeting_id, assesment.metric_id;`, [id])
+        return knex.raw(`select meetings.recurring_id,assesment.metric_id, avg(individual_avg) as cumulative_avg
+        from (select assesment.meeting_id, assesment.metric_id, avg(metric_value) as individual_avg
+                from assesment 
+                  where assesment.meeting_id in (select distinct meeting_id from user_meeting where user_meeting.user_id=?)
+                group by assesment.metric_id, assesment.meeting_id
+                order by assesment.meeting_id, assesment.metric_id) as individual_meetings inner join meetings on individual_meetings.meeting_id=meetings.meeting_id
+                inner join assesment on meetings.meeting_id=assesment.meeting_id
+                group by meetings.recurring_id, assesment.metric_id;`, [id])
     },
 
     /*deleteAssessment(knex, id) {
